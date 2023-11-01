52c1f6ed08eb832b0e0bbf21006ecf08db9a7209
Yaroslav Kovalev <kovalev.ya@promail366.com>
2023-11-01 10:34:20 +0300

    add hw-1

---
+++ lesson_01/practice/task_03/script.js
@@ -0,0 +1,13 @@
+const C = 10;
+const D = 2;
+
+const SUM = C + D;
+const DIFF = C - D;
+const MULT = C * D;
+const DIVIDE = C / D;
+
+console.log(C, D, SUM, DIFF, MULT, DIVIDE);
+console.log(C + D + SUM + DIFF + MULT + DIVIDE);
+
+
+
---
+++ lesson_01/practice/task_06/script.js
@@ -0,0 +1,15 @@
+const USER_NAME = 'Ярослав';
+console.log(USER_NAME);
+
+const JAVA = 'Java';
+const SCRIPT = 'Script';
+
+console.log(JAVA + SCRIPT);
+console.log(`${JAVA}${SCRIPT}`);
+
+const HELLO = 'Hello';
+const WORLD = 'World';
+
+console.log(HELLO + ' ' + WORLD);
+console.log(`${HELLO} ${WORLD}`);
+
