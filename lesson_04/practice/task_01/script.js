2b36b81a00669175038dc912c114d3f32e63d578
Yaroslav Kovalev <kovalev.ya@promail366.com>
2023-11-03 16:25:40 +0300

    Start HW 4

---
+++ lesson_04/practice/task_01/script.js
@@ -0,0 +1,25 @@
+// 1.
+
+const numbers = [1, 2, 3]
+numbers.forEach((number) => console.log(number));
+
+// 2.
+
+const randomArr = ['string', 2, true];
+
+console.log(randomArr.length)
+
+// 3.
+
+let arr = ['a', 'b', 'c'];
+
+// Прямое обращение к элементам массива для изменения значений
+arr[0] = 1;
+arr[1] = 2;
+arr[2] = 3;
+
+// Выводим измененный массив в консоль
+console.log(arr);
+
+
+
---
+++ lesson_04/practice/task_02/script.js
@@ -0,0 +1,25 @@
+// 1.
+let numberArr = [1, 2, 3];
+for (let i = 0; i < numberArr.length; i++) {
+  numberArr[i]++;
+}
+console.log(numberArr);
+
+const arr = [];
+arr[3] = 'a';
+arr[8] = 'b';
+
+console.log(arr.length);
+
+const numberArr2 = [1, 2, 3];
+numberArr2.push(4, 5);
+
+console.log(numberArr2);
+
+const randomArr = ['qwer', 321, false];
+
+randomArr.shift()
+
+
+
+
