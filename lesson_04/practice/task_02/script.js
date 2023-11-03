2a32f4223d8304ea89988014120197b9872bde2a
kovaldos <ya.kovalev@ya-kovalev.ru>
2023-11-03 21:46:19 +0300

    Continue hw 4

--- lesson_04/practice/task_02/script.js
+++ lesson_04/practice/task_02/script.js
@@ -5,20 +5,24 @@ for (let i = 0; i < numberArr.length; i++) {
 }
 console.log(numberArr);
 
+// 2.
 const arr = [];
 arr[3] = 'a';
 arr[8] = 'b';
 
 console.log(arr.length);
 
+// 3.
 const numberArr2 = [1, 2, 3];
 numberArr2.push(4, 5);
 
 console.log(numberArr2);
 
-const randomArr = ['qwer', 321, false];
+const randomArr = ['qwer', 321, false, 123];
 
-randomArr.shift()
+randomArr.splice(1, 2);
+
+console.log(randomArr.length);
 
 
 
